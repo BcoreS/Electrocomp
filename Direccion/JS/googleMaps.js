eeea2bc1f777110d88228909ab000442b2683cd0
AbrahamA05 <aaas06072005@gmail.com>
2025-12-01 01:10:00 -0600

    api de weatherapi en direccion

--- Direccion/JS/googleMaps.js
+++ Direccion/JS/googleMaps.js
@@ -34,3 +34,33 @@ function initMap() {
         alert("Tu navegador no soporta geolocalización.");
       }
     }
+
+
+    //wheather api 
+
+// Función para consumir la API de OpenWeatherMap
+function obtenerClima() {// Reemplaza con tu API Key
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=10.0722&lon=-84.3111&appid=fcb999651861374dfdcd703b90d25923&units=metric&lang=es`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            mostrarClima(data);
+        })
+        .catch(error => console.error("Error al obtener clima:", error));
+}
+
+// Función para mostrar datos del clima en el HTML
+
+function mostrarClima(data) {
+    document.getElementById("city").textContent = `Clima en ${data.name}`;
+    document.getElementById("icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.getElementById("temp").textContent = `${data.main.temp} °C`;
+    document.getElementById("humidity").textContent = `${data.main.humidity}%`;
+    document.getElementById("wind").textContent = `${data.wind.speed} m/s`;
+    document.getElementById("weather").innerHTML = tablaHTML;
+}
+
+
+// Ejecutar al cargar la página
+window.onload = obtenerClima;
